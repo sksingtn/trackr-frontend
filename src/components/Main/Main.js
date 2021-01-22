4b23b75cd02c488a6a83b44786b6d7d5d8ea2ca3
Shiv Kumar Singh <shivaboss5552@gmail.com>
2021-01-23 01:00:40 +0530

    Prototype of Admin Batch Section Completed

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main__body">
+        <div className="sidebar"></div>
+        <section className="right">
+          <span className="test">Lorem ipsum dolor sit amet.</span>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
