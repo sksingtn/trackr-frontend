f756ffe7939617f77686b0dc1137b3d328d533e4
sksingtn <shiv.k.singh@outlook.com>
2021-06-15 20:36:03 +0530

    Sidebar is now dynamic

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppContainer } from "../Containers/Containers";
-import Sidebar from "../Sidebar/Sidebar";
+import Sidebar, { SidebarLink } from "../Sidebar/Sidebar";
 import AdminDashboard from "../AdminDashboard/AdminDashboard";
 import AdminManage from "../AdminManage/AdminManage";
 import Notification from "../Notification/Notification";
@@ -9,7 +9,28 @@ import { Switch, Route } from "react-router-dom";
 function Admin() {
   return (
     <AppContainer>
-      <Sidebar />
+      <Sidebar>
+        <SidebarLink
+          to="/dashboard"
+          icon={<i className="fas fa-chart-area" />}
+          text="Dashboard"
+        />
+        <SidebarLink
+          to="/manage"
+          icon={<i className="fas fa-cog" />}
+          text="Manage"
+        />
+        <SidebarLink
+          to="/activity"
+          icon={<i class="fas fa-bullhorn" />}
+          text="Activity"
+        />
+        <SidebarLink
+          to="/profile"
+          icon={<i class="fas fa-address-card" />}
+          text="Profile"
+        />
+      </Sidebar>
 
       <Switch>
         <Route exact path="/dashboard">
